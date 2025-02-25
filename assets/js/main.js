2b4989f9ba9e9dac0187d968689e69e5ccef7f51
rijumia <riju.mia2612@gmail.com>
2025-02-26 00:38:24 +0600

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,13 @@
 //         header.classList.remove("sticky");
 //     }
 // });
+
+
+// setInterval(function displayDateTime() {
+//     const currentDateTime = new Date();
+//     const date = currentDateTime.toLocaleDateString();
+//     const time = currentDateTime.toLocaleTimeString();
+//     document.getElementById("datetime").innerHTML = `Time: ${time} <br> Date: ${date}`;
+// },1000);
+
+// displayDateTime();
